aa05ddc1417e5b8bb2c25487b6a6fdf0f26b5593
Jean Claude Nzabanterura <nzabajp@gmail.com>
2022-06-13 22:25:14 +0200

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='body-div' >
+            <h2>About</h2>
+            <p>I am currently on an exciting path to becoming a full stack developer. 
+                I come from a Commerce background but my love for software development
+                has pushed me to getting into this exciting field.
+            </p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Interests.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Interests = () => {
+    return (
+        <div className='body-div' >
+            <h2>Interests</h2>
+            <ul>
+                <li>Cars</li>
+                <li>JS</li>
+                <li>React</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Interests;
